b683676bfc539790afbceaaa25a4d807ea1f9ff3
victor <victor@192.168.162.1>
2021-09-28 04:25:16 +0200

    TEW-2022-309-2.2-Servelts con sesiones.

---
+++ servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -0,0 +1,71 @@
+package com.tew.Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundoServlet
+ */
+@WebServlet(name = "HolaMundo", urlPatterns = { "/HolaMundoCordial" })
+public class HolaMundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaMundoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = (String) request.getParameter("NombreUsuario");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+		out.println("<BODY>");if ( nombre != null ){
+			out.println("<br>Hola "+nombre+"<br>");
+			}
+		out.println("Bienvenido a mi primera página Web!");
+		out.println("</BODY></HTML>");
+		
+		@SuppressWarnings("unchecked")
+		Vector<String> listado = (Vector<String>)request.getSession().getAttribute("listado");
+		if (listado == null){
+			listado = new Vector<String>();
+			}
+		if ( nombre != null ){
+			out.println("<br>Hola "+nombre+"<br>");
+			listado.addElement(nombre);
+			}
+		request.getSession().setAttribute("listado",listado);
+		out.println("Bienvenido a mi primera página web!");
+		out.println("<br>");
+		out.println("Contigo, hoy me han visitado:<br>");
+		for ( int i = 0 ; i < listado.size() ; i++ ){
+			out.println("<br>"+(String)listado.elementAt(i));
+			}
+		out.println("<a href=\"index.html\">volver</a>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
