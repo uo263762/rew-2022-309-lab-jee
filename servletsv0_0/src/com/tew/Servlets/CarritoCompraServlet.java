24ba69ef868861930b0dfa839c8f7febb1bc1d7d
victor <victor@192.168.162.1>
2021-09-29 04:38:52 +0200

    TEW-2022-309-2.11-Ejercicios tienda de la compra con Servlets v2

--- servletsv0_0/src/com/tew/Servlets/CarritoCompraServlet.java
+++ servletsv0_0/src/com/tew/Servlets/CarritoCompraServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -50,36 +51,10 @@ public class CarritoCompraServlet extends HttpServlet {
 		}
 		
 		request.getSession().setAttribute("carrito", carrito);
-
-		String html = "<!DOCTYPE html>\r\n" + 
-				"<html>\r\n" + 
-				"<body>\r\n" + 
-				" <h2>Tienda de peliculas</h2>\r\n" + 
-				"  <form action=\"/servletsv0_0/CarritoCompraServlet\">\r\n" + 
-				"   <select name=\"product_id\" size=1>\r\n" + 
-				"    <option value=\"0\">Porrillo</option>\r\n" + 
-				"    <option value=\"1\">Matrix 2</option>\r\n" + 
-				"    <option value=\"2\">Junaito</option>\r\n" + 
-				"    <option value=\"3\">Canelita</option>\r\n" + 
-				"    <option value=\"4\">CNI</option>\r\n" + 
-				"    <option value=\"5\">Mosad</option>\r\n" + 
-				"    <option value=\"6\">NSA</option>\r\n" + 
-				"    <option value=\"7\">KGB</option>\r\n" + 
-				"    <option value=\"8\">TEA</option>\r\n" + 
-				"    <option value=\"9\">DEA</option>\r\n" + 
-				"   </select>\r\n" + 
-				"    <input type=\"submit\" value=\"Añadir\">\r\n" + 
-				"  </form>\r\n" + 
-				" <hr>\r\n" + 
-				" <h2>Carrito</h2>\r\n";
-	
-		for (String id : carrito.keySet()) {
-			html += "<br> ID: " + id + " --> Amount: " + carrito.get(id) + "</br>";
-		}
-		html += "</body>\r\n" + 
-				"</html>";
-		PrintWriter out = response.getWriter();
-		out.println(html);
+		
+		RequestDispatcher dispatcher = 
+				getServletContext().getNamedDispatcher("CarritoCompraVistaServlet");
+		dispatcher.forward(request, response);
 	}
 
 	/**
