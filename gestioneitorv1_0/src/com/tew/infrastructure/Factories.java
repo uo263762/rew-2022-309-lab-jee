371c74634d5e32a311974210db291ef3ae9d78d9
Pau amore <Pau amore@DESKTOP-FRJKEML>
2021-10-04 20:37:31 +0200

    TEW-2022-309-3.2-Managed Bean BeanAlumnos acceso a la capa de servicio

--- gestioneitorv1_0/src/com/tew/infrastructure/Factories.java
+++ gestioneitorv1_0/src/com/tew/infrastructure/Factories.java
@@ -1,23 +1,25 @@
 package com.tew.infrastructure;
 
+import com.tew.business.AlumnosService;
+import com.tew.business.ServicesFactory;
+import com.tew.model.Alumno;
+import com.tew.persistence.PersistenceFactory;
+
+import impl.tew.*;
 import impl.tew.business.SimpleServicesFactory;
 import impl.tew.persistence.SimplePersistenceFactory;
 
-import com.tew.persistence.PersistenceFactory;
-import com.tew.business.ServicesFactory;
-
 /**
- * Esta clase es la que realemente relaciona las interfaces de las capas 
- * con sus implementaciones finales. Si se deben hacer cambios de implementaciï¿½ï¿½n
- * en algunas de las capas habrï¿½ï¿½a que retocar esta clase.
+ * Esta clase es la que realemente relaciona las interfaces de las capas con sus
+ * implementaciones finales. Si se deben hacer cambios de implementaciï¿½ï¿½n en
+ * algunas de las capas habrï¿½ï¿½a que retocar esta clase.
  * 
- * En desarrollos mas sofisticados esto se especificarï¿½ï¿½n en ficheros de 
- * configuraciï¿½ï¿½n lo que permitiria al "assembler" y "deployer" ajustar los 
- * ensamblajes entre capas sin necesidad de recompilar.
- * 		Assembler: forma la aplicaciï¿½ï¿½n a base de componentes 
- * 			desarrollados externamente
- * 		Deployer: Adapta la aplicaciï¿½ï¿½n, o reconfigura la aplicaciï¿½ï¿½n en 
- * 			explotaciï¿½ï¿½n a las mï¿½ï¿½quinas/contenedores (tiers/layers)
+ * En desarrollos mas sofisticados esto se especificarï¿½ï¿½n en ficheros de
+ * configuraciï¿½ï¿½n lo que permitiria al "assembler" y "deployer" ajustar los
+ * ensamblajes entre capas sin necesidad de recompilar. Assembler: forma la
+ * aplicaciï¿½ï¿½n a base de componentes desarrollados externamente Deployer:
+ * Adapta la aplicaciï¿½ï¿½n, o reconfigura la aplicaciï¿½ï¿½n en
+ * explotaciï¿½ï¿½n a las mï¿½ï¿½quinas/contenedores (tiers/layers)
  * 
  * Hay frameworks especializados en eso precisamente, por ejemplo Spring.
  * 
@@ -29,4 +31,54 @@ public class Factories {
 	public static ServicesFactory services = new SimpleServicesFactory();
 	public static PersistenceFactory persistence = new SimplePersistenceFactory();
 
+	public String listado() {
+		AlumnosService service;
+		try {
+			// Acceso a la implementacion de la capa de negocio
+			// a través de la factoría
+			service = Factories.services.createAlumnosService();
+			// Asi le damos información a toArray para poder hacer el casting a Alumno[]
+			alumnos = (Alumno[]) service.getAlumnos().toArray(new Alumno[0]);
+			return "exito";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+
+	public String edit() {
+		AlumnosService service;
+		try {
+			// Acceso a la implementacion de la capa de negocio
+			// a través de la factoría
+			service = Factories.services.createAlumnosService();
+			// Recargamos el alumno en la tabla de la base de datos por si hubiera cambios.
+			alumno = service.findById(alumno.getId());
+			return "exito";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+
+	public String salva() {
+		AlumnosService service;
+		try {
+		// Acceso a la implementacion de la capa de negocio
+			// a través de la factoría
+			service = Factories.services.createAlumnosService();
+			//Salvamos o actualizamos el alumno segun sea una operacion de alta o de edición
+			if (alumno.getId() == null) {
+			service.saveAlumno(alumno);
+			}
+			else {
+			service.updateAlumno(alumno);
+			}
+			//Actualizamos el javabean de alumnos inyectado en la tabla
+			alumnos = (Alumno [])service.getAlumnos().toArray(new Alumno[0]);
+			return "exito";
+			} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+			}
 }
