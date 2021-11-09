690a00774f9c524756fbd9f27fc6f7297e4bd787
UO263762 <UO263762@DESKTOP-A9MB9D4.mshome.net>
2021-11-09 14:40:07 +0100

    TEW-309-7.2-Servicios: Anotación del DTO

--- gestioneitorv4_0/src/com/tew/model/Alumno.java
+++ gestioneitorv4_0/src/com/tew/model/Alumno.java
@@ -1,5 +1,9 @@
 package com.tew.model;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "alumno")
 public class Alumno {
 	private Long id;
 	private String nombre;
@@ -7,35 +11,60 @@ public class Alumno {
 	private String iduser;
 	private String email;
 
-	public String getNombre() {
-		return nombre;
+	public Alumno() {
 	}
+
+	public Alumno(Long id, String nombre, String apellidos, String iduser, String email) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.iduser = iduser;
+		this.email = email;
+	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getApellidos() {
-		return apellidos;
-	}
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-	public String getIduser() {
-		return iduser;
-	}
+
 	public void setIduser(String iduser) {
 		this.iduser = iduser;
 	}
-	public String getEmail() {
-		return email;
-	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	@XmlElement
+	public String getNombre() {
+		return nombre;
+	}
+
+	@XmlElement
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	@XmlElement
+	public String getIduser() {
+		return iduser;
+	}
+
+	@XmlElement
+	public String getEmail() {
+		return email;
+	}
+
+	@XmlElement
 	public Long getId() {
 		return id;
 	}
+
 }
