a341235d64428fbf8e4693561142f3c4ca7aa5c1
UO263762 <UO263762@DESKTOP-A9MB9D4.mshome.net>
2021-11-23 15:25:14 +0100

    TEW - 309 – 9.1 Model – Método Edit alumno

---
+++ gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+
+// Clase que contiene el Modelo de la aplicación.
+function Model() {
+	// Lista de alumnos.
+	this.tbAlumnos = null;
+
+	// Carga los datos del servicio sobreescribiendo el dato this.tbAlumnos.
+	this.load = function() {
+		this.tbAlumnos = AlumnosServicesRs.getAlumnos();
+	}
+
+	// Llamamos al servicio de alta de alumno
+	this.add = function(alumno) {
+		// Llamamos al servicio de alta de alumno
+		AlumnosServicesRs.saveAlumno({
+			$entity : alumno,
+			$contentType : "application/json"
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+
+	// Actualización de un alumno existente: PENDIENTE DE IMPLEMENTAR
+	this.edit = function(alumno) {
+	}
+
+	// Eliminación un alumno existente
+	this.remove = function(id_alumno) {
+		// Llamamos al servicio de borrado de alumno
+		AlumnosServicesRs.deleteAlumno({
+			id : id_alumno
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+
+	this.find = function(id_alumno) {
+		function checkAlumno(obj) {
+			return obj.id == id_alumno;
+		}
+		// Buscamos los datos del alumno cuyo id_alumno sea el mismo que el
+		// seleccionado
+		var alumno = this.tbAlumnos.find(checkAlumno);
+		return alumno;
+	}
+};
